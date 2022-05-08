9118003ae6279e6a8cae6978d40062e9cf04e7a9
bhavansh-arora <bhavansharora21@gmail.com>
2022-05-08 13:35:01 +0530

    first commit

---
+++ query/connexion.php
@@ -0,0 +1,3 @@
+<?php
+    $con = mysqli_connect('db-mysql-nyc3-72492-do-user-10969009-0.b.db.ondigitalocean.com','doadmin','AVNS_5JlCeiFXObmDysJ','bd_on_demand_taxi') or die('unable to connect to db');
+?>
